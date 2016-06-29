6d395d9dce48ba4a2a6fd1cdf9e8b7e55584548f
Matthew Pennell <mwpennell@dhcp-128-189-209-81.ubcsecure.wireless.ubc.ca>
2016-06-29 14:51:33 -0700

    updated summary stats

--- R/summary.R
+++ R/summary.R
@@ -1,7 +1,20 @@
 #' Calculate bcr at equilbrium
 #' Type I Functional Response
-#' @export bcm_eqm_1
+#' @export bcr_eqm_1
 #'
-bcr_eqm_1 <- function(K, a, m, e)
+bcr_eqm_1 <- function(m, a, e, K)
     K * a * e / m
 
+#' Equilibrium biomass for resource
+#' Type I Functional Response
+#' @export biomass_res_1
+#'
+biomass_res_1 <- function(m, a, e)
+    m / (e * a)
+
+#' Equilibrium biomass for consumer
+#' Type I Functional Response
+#' @export biomass_con_1
+#'
+biomass_con_1 <- function(m, a, e, r, K)
+    r / a * (1 - m / (e * a * K))
